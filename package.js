698adeb0b41480b818ae713b1ad5e64c21bca429
caioreis <caio@cyclumsn.com>
2014-08-16 05:18:41 -0300

    Correcting erros

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   summary: "jQuery unveil library"
 });
 
-Package.on_test(function (api) {
+Package.on_use(function (api) {
   api.use('jquery', 'client');
   api.add_files('jquery-unveil.js', 'client');
 });
